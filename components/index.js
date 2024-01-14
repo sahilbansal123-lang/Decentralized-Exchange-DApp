1a2548e9309199509318a6bd46ec62be6bdaa4ea
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-01-14 10:18:10 +0530

    SETTING UP IMPORTS

---
+++ components/index.js
@@ -0,0 +1,36 @@
+import Card from "./Card";
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Selector from "./Selector";
+import SingleCard from "./SingleCard";
+import SwapComponet from "./SwapComponent";
+import SwapField from "./SwapField";
+import Table from "./Table";
+import TokenBalance from "./TokenBalance";
+import TransactionStatus from "./TransactionStatus";
+// SVG ICONS
+import FaceBook from "./SVG/Facebook";
+import Insta from "./SVG/Insta";
+import Logo from "./SVG/Logo";
+import Menu from "./SVG/Menu";
+import Twitter from "./SVG/Twitter";
+
+export {
+  Card,
+  Footer,
+  Header,
+  HeroSection,
+  Selector,
+  SingleCard,
+  SwapComponet,
+  SwapField,
+  Table,
+  TokenBalance,
+  TransactionStatus,
+  FaceBook,
+  Insta,
+  Logo,
+  Menu,
+  Twitter,
+};
