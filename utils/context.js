a5959e0d84e3517f7791d9827c9bc05549788f90
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-01-17 12:00:56 +0530

    context

--- utils/context.js
+++ utils/context.js
@@ -1,3 +1,125 @@
+import { BigNumber, ethers } from "ethers";
+import { contract, tokenContract } from "./contract";
+import { toEth, toWei } from "./utils";
 
+export async function swapEthToToken(tokenName, amount) {
+  try {
+    let tx = { value: toWei(amount) };
 
+    const contractObj = await contract();
+    const data = await contractObj.swapEthToToken(tokenName, tx);
 
+    const reciept = await data.wait();
+    return reciept;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function hasValidAllowance(owner, tokenName, amount) {
+  try {
+    const contractObj = await contract();
+    const address = await contractObj.getTokenAddress(tokenName);
+
+    const tokenContractObj = await tokenContract(address);
+    const data = await tokenContractObj.allowance(
+      owner,
+      "0x1776893d9973262154d0b18C27ceeeFc6865bA47"
+    );
+
+    const result = BigNumber.from(data.toString()).gte(
+      BigNumber.from(toWei(amount))
+    );
+
+    return result;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function swapTokenToEth(tokenName, amount) {
+  try {
+    const contractObj = await contract();
+    const data = await contractObj.swapTokenToEth(tokenName, toWei(amount));
+
+    const reciept = await data.wait();
+    return reciept;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function swapTokenToToken(srcToken, destToken, amount) {
+  try {
+    const contractObj = await contract();
+    const data = await contractObj.swapTokenToToken(
+      srcToken,
+      destToken,
+      toWei(amount)
+    );
+    const reciept = await data.wait();
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function getTokenBalance(tokenName, address) {
+  const contractObj = await contract();
+  const balance = contractObj.getBalance(tokenName, address);
+  return balance;
+}
+
+export async function getTokenAddress(tokenName) {
+  try {
+    const contractObj = await contract();
+    const address = contractObj.getTokenAddress(tokenName);
+    return address;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function increraseAllowance(tokenName, amount) {
+  try {
+    const contractObj = await contract();
+    const address = contractObj.getTokenAddress(tokenName);
+    const tokenContractObj = await tokenContract(address);
+    const data = await tokenContractObj.approve(
+      "0x1776893d9973262154d0b18C27ceeeFc6865bA47",
+      toWei(amount)
+    );
+
+    const reciept = await data.wait();
+    return reciept;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+export async function getAllHistory() {
+  try {
+    const contractObj = await contract();
+    const getAllHistory = contractObj.getAllHistory(tokenName);
+    const historyTransaction = getAllHistory.map((history, i) => ({
+      historyId: history.historyId.toNumber(),
+      tokenA: history.tokenA,
+      tokenB: history.tokenB,
+      inputValue: toEth(history?.inputValue),
+      outputValue: toEth(history?.outputValue),
+      userAddress: history.userAddress,
+    }));
+    return historyTransaction;
+  } catch (e) {
+    return parseErrorMsg(e);
+  }
+}
+
+function toWei(amount) {
+  const toWei = ethers.utils.parseUnits(amount.toString());
+  return toWei.toString();
+}
+
+function parseErrorMsg(e) {
+  const json = JSON.parse(JSON.stringify(e));
+  return json?.reason || json?.error?.message;
+}
