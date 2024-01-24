25bfbec832b12376fd9ee74117eab487e8360bf0
sahilbansal123-lang <bansalsahil9053@gmail.com>
2024-01-24 11:11:05 +0530

    Single card

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -158,7 +158,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc69320fd356b768"; }
+/******/ 		__webpack_require__.h = function() { return "da572a0fb8467d3d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
